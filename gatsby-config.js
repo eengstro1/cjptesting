d11ecc5abc6c901d7119357c6bad3f0f020f6ddb
erik engstrom <erik@engstromconsulting.com>
2020-04-10 00:55:56 -0500

    changed to snipcartv3

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,10 +23,10 @@ module.exports = {
       options: { apiToken: process.env.DATO_API_TOKEN },
     },
     {
-      resolve: 'gatsby-plugin-snipcart',
+      resolve: 'gatsby-plugin-snipcartv3',
       options: {
         apiKey: process.env.SNIPCART_API_TOKEN,
-        autopop: false,
+        autopop: true,
         js: 'https://cdn.snipcart.com/themes/v3.0.0/default/snipcart.js',
         styles: 'https://cdn.snipcart.com/themes/v3.0.0/default/snipcart.css'
       }
